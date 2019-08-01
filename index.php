752452eed6ad7708767013be4352cece668c151c
fermartinotti <fermartinotti@gmail.com>
2019-08-01 10:58:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-print "LGI! php version is " . PHP_VERSION . "\n";
+print "LGI! php version is ";
 print "En este y en el oeste\n";
 print "En el norte y en el sur";
 print "Brillara blanca y celeste \n";
