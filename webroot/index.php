e26f029fbde8d62519065616406047041b0e2e25
fermartinotti <fermartinotti@gmail.com>
2019-08-01 12:09:10 -0300

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,7 +1,7 @@
 <?php
-print "LGI! php version is ";
+print "LGI! \n";
 print "En este y en el oeste\n";
-print "En el norte y en el sur";
+print "En el norte y en el sur\n";
 print "Brillara blanca y celeste \n";
 print "La academia racing club \n";
 ?>
